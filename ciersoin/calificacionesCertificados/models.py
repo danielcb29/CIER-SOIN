a10b142b2dd044a1966b790ada05f94bed7883f3
Daniel Correa <danielc.web@gmail.com>
2015-03-07 17:02:16 -0500

    Primeros modelos implementados, modulo de certificadosCalificaciones

--- ciersoin/calificacionesCertificados/models.py
+++ ciersoin/calificacionesCertificados/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from calificacionesCertificados import LeaderTeacher
+from cursosCohortesActividades import Cohorte
 # Create your models here.
+
+class Certificado(models.Model):
+    leader_teacher = models.ForeignKey(LeaderTeacher)
+
+    #Choices de tipo
+    ASISTIO='AS'
+    PARTICIPO='PA'
+    EXCELENCIA='EX'
+
+    TIPO_CERTIFICADO_CHOICES = (
+        (ASISTIO,'Asistio'),
+        (PARTICIPO,'Participo'),
+        (EXCELENCIA,'EXCELENCIA'),
+    )
+
+    tipo = models.CharField(max_length=3,choices=TIPO_CERTIFICADO_CHOICES,default=ASISTIO)
+
+    cohorte = models.ForeignKey(Cohorte)
+
+class Calificacion(models.Model):
+    notas = models.CharField(max_length=35)
+    cohorte = models.ForeignKey(Cohorte)
+    leader_teacher = models.ForeignKey(LeaderTeacher)
