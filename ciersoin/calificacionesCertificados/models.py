ec7ffd712215a0ccac4abc87abf134fc4894760c
Daniel Correa <danielc.web@gmail.com>
2015-03-16 23:44:22 -0500

    Landing Page implementada, se presenta la pagina para el publico general con registro de LT, faltan acomodar detalles minimos para tenerla terminada

--- ciersoin/calificacionesCertificados/models.py
+++ ciersoin/calificacionesCertificados/models.py
@@ -7,14 +7,14 @@ class Certificado(models.Model):
     leader_teacher = models.ForeignKey(LeaderTeacher)
 
     #Choices de tipo
-    ASISTIO='AS'
-    PARTICIPO='PA'
-    EXCELENCIA='EX'
+    ASISTIO='Asistio'
+    PARTICIPO='Participo'
+    EXCELENCIA='Excelencia'
 
     TIPO_CERTIFICADO_CHOICES = (
         (ASISTIO,'Asistio'),
         (PARTICIPO,'Participo'),
-        (EXCELENCIA,'EXCELENCIA'),
+        (EXCELENCIA,'Excelencia'),
     )
 
     tipo = models.CharField(max_length=3,choices=TIPO_CERTIFICADO_CHOICES,default=ASISTIO)
--- ciersoin/ciersoin/views.py
+++ ciersoin/ciersoin/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request,'login.html',{})
+
+def index(request):
+    return render(request,'index.html',{})
