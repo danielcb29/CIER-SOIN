3aa8325abee01663fdd071c53295ee9bb9633c7f
Daniel Correa <danielc.web@gmail.com>
2015-03-29 23:58:31 -0500

    consulta de disponibilidad de cursos terminado
     Se implementa el patron Iterator para realizar la busqueda de cursos
    ofertados para un LT teniendo en cuenta que no puede repetir cursos y
    que solo se deben mostrar los referentes a su area. Terminado y
    funcionando bien. Falta implementar la matricula de dicho curso, es
    decir, tener en cuenta que en el proceso de matricula debe haber
    presupuesto y todo lo que implica ello. Falta la parte de
    aspirantes_cursos para poder realizar la activación y matricula de
    aspirantes a varios cursos. Revisar modelos para poder solucionar dicho
    problema

--- ciersoin/teacher/views.py
+++ ciersoin/teacher/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .forms import LeaderTeacherForm
 from .teacherfactory import TeacherFactory
 from django.contrib.auth.hashers import make_password,is_password_usable
+from .models import LeaderTeacher
+from cursosCohortesActividades.cursoiterator import CursoIterator
 # Create your views here.
 
 def registro_lt(request):
@@ -18,4 +20,30 @@ def registro_lt(request):
             lt_form = LeaderTeacherForm()
             exito = True
     return render(request,'index.html',{'form':lt_form,'exito':exito})
+
+def oferta_cursos_lt(request):
+    lt_form = LeaderTeacherForm()
+    error = ""
+    oferta = False
+    area = ""
+    cursos = []
+    disponibilidad_cursos = False
+    if request.method == 'POST':
+        cc = request.POST['cc']
+        lt = LeaderTeacher.objects.filter(cedula=cc)
+        if len(lt) == 0:
+            error = 'No se encuentra matriculado aun, debe primero realizar el registro'
+        else:
+            oferta = True
+            lt_obj = lt[0]
+            area = lt[0].area_interes
+            cursos_iterator = CursoIterator(area,lt_obj)
+
+            if cursos_iterator.len() != 0:
+                disponibilidad_cursos = True
+                while not cursos_iterator.is_done():
+                    cursos.append(cursos_iterator.next())
+
+    return render(request,'index.html',{'form':lt_form,'oferta':oferta,'error':error,'area':area,'cursos':cursos,'disponibilidad':disponibilidad_cursos})
+
 
