ef700b9c7b4d1aa488664cc0881b248d6328380d
Daniel Correa <danielc.web@gmail.com>
2015-04-03 20:54:53 -0500

    edit de lt y mt terminado
    editar de lt y mt terminado y completo tanto datos personales como
    historia academica y laboral. Faltan validaciones de ingreso indebido
    por url y url e ids que no existen

--- ciersoin/historiaAcademicaLaboral/urls.py
+++ ciersoin/historiaAcademicaLaboral/urls.py
@@ -4,10 +4,10 @@ from .views import *
 urlpatterns = patterns('',
     url(r'^academica',nueva_academica),
     url(r'^laboral',nueva_laboral),
-    url(r'^eliminar/ha/(\d+)',eliminar_ha),
-    url(r'^editar/ha/(\d+)',editar_ha),
-    url(r'^eliminar/hl/(\d+)',eliminar_hl),
-    url(r'^editar/hl/(\d+)',editar_hl),
+    url(r'^eliminar/ha/(\d+)',eliminar_ha,name='eliminar_ha'),
+    url(r'^editar/ha/(\d+)',editar_ha,name='editar_ha'),
+    url(r'^eliminar/hl/(\d+)',eliminar_hl,name='eliminar_hl'),
+    url(r'^editar/hl/(\d+)',editar_hl,name='editar_hl'),
 
 
 )
--- ciersoin/historiaAcademicaLaboral/views.py
+++ ciersoin/historiaAcademicaLaboral/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .forms import HistoriaAcademicaForm,HistoriaLaboralForm
-#from teacher.teacherfactory import TeacherFactory
 from django.contrib.auth.decorators import login_required,permission_required
 from .models import HistoriaAcademica,HistoriaLaboral
 from django.http import HttpResponseRedirect
+from teacher.models import Teacher
+from teacher.forms import TeacherEditForm
 # Create your views here.
 @login_required
 def nueva_academica(request):
@@ -33,7 +34,23 @@ def nueva_laboral(request):
 
 @login_required
 def editar_ha(request,id):
-    pass
+    editar = True
+    ha = HistoriaAcademica.objects.get(id=id)
+    ha_form = HistoriaAcademicaForm(instance=ha,initial=ha.__dict__)
+    id_user = request.user.id
+    teach = Teacher.objects.get(id=id_user)
+    form = TeacherEditForm(instance=teach)
+    historiasacademicas = HistoriaAcademica.objects.filter(teacher=teach)
+    historiaslaborales = HistoriaLaboral.objects.filter(teacher=teach)
+    exito_ha=False
+    if request.method=='POST':
+        form_nuevo = HistoriaAcademicaForm(request.POST,instance=ha,initial=ha.__dict__)
+        if form_nuevo.has_changed():
+            if form_nuevo.is_valid():
+                form_nuevo.save()
+                exito_ha = True
+                editar=False
+    return render(request,'edit_teach.html',{'editar':editar,'type':'Historia Academica','form_history':ha_form,'form':form,'historiaacademica':historiasacademicas,'historialaboral':historiaslaborales,'exito_ha':exito_ha})
 
 @login_required
 def eliminar_ha(request,id):
@@ -47,7 +64,23 @@ def eliminar_ha(request,id):
 
 @login_required
 def editar_hl(request,id):
-    pass
+    editar = True
+    hl = HistoriaLaboral.objects.get(id=id)
+    hl_form = HistoriaLaboralForm(instance=hl,initial=hl.__dict__)
+    id_user = request.user.id
+    teach = Teacher.objects.get(id=id_user)
+    form = TeacherEditForm(instance=teach)
+    historiasacademicas = HistoriaAcademica.objects.filter(teacher=teach)
+    historiaslaborales = HistoriaLaboral.objects.filter(teacher=teach)
+    exito_hl=False
+    if request.method=='POST':
+        form_nuevo = HistoriaLaboralForm(request.POST,instance=hl,initial=hl.__dict__)
+        if form_nuevo.has_changed():
+            if form_nuevo.is_valid():
+                form_nuevo.save()
+                exito_hl = True
+                editar=False
+    return render(request,'edit_teach.html',{'editar':editar,'type':'Historia Academica','form_history':hl_form,'form':form,'historiaacademica':historiasacademicas,'historialaboral':historiaslaborales,'exito_hl':exito_hl})
 
 @login_required
 def eliminar_hl(request,id):
