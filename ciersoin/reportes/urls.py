dbfa05a9ec5de313a703704f069c6675c12d9f62
Daniel Correa <danielc.web@gmail.com>
2015-04-09 22:01:34 -0500

    Template Reportes gráficos lista
    Template estatica funcionando con datos dinámicos comentados. Esta la
    primera version de los listados. Esto con el fin de la facil
    integración con el patron fachada

--- ciersoin/reportes/urls.py
+++ ciersoin/reportes/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'^$',dashboards),
+    url(r'^graficos$',dashboards),
+    url(r'^listados$',listados),
 )
--- ciersoin/reportes/views.py
+++ ciersoin/reportes/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dashboards(request):
     return render(request,'reportes.html',{})
+
+def listados(request):
+    return render(request,'listados.html',{})
