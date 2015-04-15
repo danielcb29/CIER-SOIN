7938c8940f24e01f5307cdb7e47e752204bd90c0
fermat986 <martinez.alvaro@correounivalle.edu.co>
2015-04-14 22:13:51 -0500

    Patron de estado sin implemenar

---
+++ ciersoin/calificacionesCertificados/estadoCurso.py
@@ -0,0 +1,22 @@
+__author__ = 'alvaro'
+from .models import Certificado
+from teacher.models import LeaderTeacher
+from cursosCohortesActividades.models import Cohortes
+
+class Contexto():
+
+    global EstadoCurso
+
+
+class EstadoCursoAprobado(EstadoCurso):
+
+    def generarCertificado(idTeacher):
+
+        return 0
+
+class EstadoCursoReprobIncomp(EstadoCurso):
+
+    def generarCertificado(idTeacher):
+
+        return 0
+
