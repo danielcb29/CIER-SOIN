60572e49fc6fac0519860652b54b7449397936d5
Daniel Correa <danielc.web@gmail.com>
2015-05-02 17:14:52 -0500

    Generacion de certificado correcta con patron state, falta implementar metodo que define si es posible generarlo o no

--- ciersoin/calificacionesCertificados/estadoCurso.py
+++ ciersoin/calificacionesCertificados/estadoCurso.py
@@ -5,17 +5,25 @@ from cursosCohortesActividades.models import Cohorte
 
 class Contexto():
 
-    global EstadoCurso
+    def existe_certficiado(self,id_teach,id_cohor):
+        lt = LeaderTeacher.objects.get(id=id_teach)
+        coh = Cohorte.objects.get(id=id_cohor)
+        cert = Certificado.objects.filter(leader_teacher=lt,cohorte=coh)
+        if len(cert) == 0:
+            return False
+        else:
+            return True
 
-class EstadoCursoAprobado():
+class EstadoCursoExiste():
 
-    def generarCertificado(idTeacher):
+    def generarCertificado(self,id_teach,id_cohor):
+        return Certificado.objects.get(leader_teacher= LeaderTeacher.objects.get(id_teach), cohorte = Cohorte.objects.get(id_cohor))
 
-        return 0
+class EstadoCursoNoExiste():
 
-class EstadoCursoReprobIncomp():
+    def posible(self,id_teach,id_cohor):
+        pass
 
-    def generarCertificado(idTeacher):
-
-        return 0
+    def generarCertificado(self,id_teach,id_cohor):
+        return Certificado.objects.get(leader_teacher= LeaderTeacher.objects.get(id_teach), cohorte = Cohorte.objects.get(id_cohor))
 
