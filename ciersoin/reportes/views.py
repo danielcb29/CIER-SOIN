b610d16b241d92c98d15a65bf2813876d12e52ef
Daniel Correa <danielc.web@gmail.com>
2015-05-06 14:20:20 -0500

    test POST en reportes graficos

--- ciersoin/reportes/views.py
+++ ciersoin/reportes/views.py
@@ -4,11 +4,19 @@ from .FachadaReporte import FachadaReporte
 # Create your views here.
 fachada = FachadaReporte()
 def dashboards(request):
-    mon_str = {0:'Enero',1:'Febrero',2:'Marzo',3:'Abril',4:'Mayo',5:'Junio',6:'Julio',7:'Agosto',8:'Septiembre',9:'Ocutbre',10:'Noviembre',11:'Diciembre'}
-    month = datetime.datetime.now().month
-    mon_spanish = mon_str[month]
-    #aproved,reporv = fachada.reportarAprobReproCurso(month) #Aprobados y reporbados ordenados por dpto
-    return render(request,'reportes.html',{'mes':mon_spanish})
+    regs = 'NIL'
+    mes = 'No Seleccionado'
+    periodo = 'No Seleccionado'
+    year = 'No Seleccionado'
+    if request.method == 'POST':
+        print 'Entramos'
+        print request.POST['mes'],request.POST['periodo'],request.POST['year']
+        '''mon_str = {0:'Enero',1:'Febrero',2:'Marzo',3:'Abril',4:'Mayo',5:'Junio',6:'Julio',7:'Agosto',8:'Septiembre',9:'Ocutbre',10:'Noviembre',11:'Diciembre'}
+        month = datetime.datetime.now().month
+        mon_spanish = mon_str[month]'''
+        #aproved,reporv = fachada.reportarAprobReproCurso(month) #Aprobados y reporbados ordenados por dpto
+
+    return render(request,'reportes.html',{'mes':mes,'periodo':periodo,'year':year,'regs':regs})
 
 def listados(request):
     return render(request,'listados.html',{})
