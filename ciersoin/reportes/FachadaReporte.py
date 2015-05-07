bd2c39a47d9678d52fe8593c3f2dfeb979ebf4a2
Daniel Correa <danielc.web@gmail.com>
2015-05-06 19:39:55 -0500

    Reportes en lista terminados y funcionando correctamente. Faltan reportes graficos y anadir visualizacion de calificaciones a mt

--- ciersoin/reportes/FachadaReporte.py
+++ ciersoin/reportes/FachadaReporte.py
@@ -1,26 +1,49 @@
 __author__ = 'alvaromartinez'
 from teacher.models import LeaderTeacher
-from cursosCohortesActividades.models import Curso,Actividad,Cohorte
+from cursosCohortesActividades.models import Curso,Actividad,Cohorte,Actividad_Cohorte
 from calificacionesCertificados.models import Calificacion,Certificado
 
 class FachadaReporte():
 
+#La	tienda	genera	unos	reportes	mensuales	en	forma	de	texto	en	tablas	de:
+
+    #Detalle	de	estudiantes	en	un	curso	por	departamentos
+    def list_est_curso_dpto(self,mes,curso,year):
+        curso = Curso.objects.get(nombre=curso)
+        cohortes = Cohorte.objects.filter(curso=curso,fecha_final__month=mes,fecha_final__year=year)
+        for c in cohortes:
+            c.est = c.estudiantes.all().order_by('departamento')
+        return cohortes
+
+    #Historico	de	estudiantes	que	hay	ganado	un	curso
+    def hist_estud_ganaron(self,mes,curso,year):
+        curso = Curso.objects.get(nombre=curso)
+        cohortes = Cohorte.objects.filter(curso=curso,fecha_final__month=mes,fecha_final__year=year)
+        for c in cohortes:
+            c.cert = Certificado.objects.filter(cohorte=c)
+        return cohortes
+
     #Definicion de la funcion para la generacion de reporte de asistentes por  un curso determinado
     def reportarAsistCurso(self, idCurso):
-        return 0
+        pass
 
     #Definicion de la funcion para la generacion de reporte de docentes por departamento
     def reportarDocentePorDepart(self, idDepart):
-        return 0
+        pass
 
     #Definicion de la funcion para la generacion de reporte de cursos con menos potencial de avance
     def reportarCursosMenosPotencial(self):
-        return 0
+        pass
 
     #Definicion de la funcion para la generacion de reporte de Porcentaje de aprobados y reprobados en un curso determinado
-    def reportarAprobReproCurso(self, mes):
+    def reportarAprobReproCurso(self,periodo,year):
         lts = LeaderTeacher.objects.all().order_by('departamento')
-        cohorte_in_month = Cohorte.objects.filter(fecha_final__month=mes,estudiantes__in= lts)
+        cohorte_in_semester = Cohorte.objects.filter(fecha_final__year=year,estudiantes__in= lts,periodo=periodo).distinct()
+        array_act_cohor = []
+        for coh in cohorte_in_semester:
+            actvi = Actividad_Cohorte.objects.filter(cohorte=coh)
+            array_act_cohor.append(actvi)
+
         aproved = Certificado.objects.filter(cohorte__in = cohorte_in_month,tipo='Excelencia').count
         reporv = Certificado.objects.filter(cohorte__in = cohorte_in_month,tipo='Asistencia').count
         i = 0
@@ -35,12 +58,12 @@ class FachadaReporte():
         return ap_percent,rp_percent
     #Definicion de la funcion para la generacion de reporte de notas de los estudiantes en un curso determinado
     def reportarNotasEstu(self, idCurso):
-        return 0
+        pass
 
     #Definicion de la funcion para la generacion de reporte de historico de los estudiantes que han ganado un curso
     def reportarHistoricoEstudi(self, idCurso):
-        return 0
+        pass
 
     #Definicion de la funcion para la generacion de reporte de datos de los estudiantes detallados y organizados por departamentos
     def reportarEstudianteDetPorDepartamento(self):
-        return 0
+        pass
--- ciersoin/reportes/views.py
+++ ciersoin/reportes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import datetime
 from .FachadaReporte import FachadaReporte
+from cursosCohortesActividades.models import Curso
 # Create your views here.
 fachada = FachadaReporte()
 def dashboards(request):
@@ -11,6 +12,7 @@ def dashboards(request):
     if request.method == 'POST':
         print 'Entramos'
         print request.POST['mes'],request.POST['periodo'],request.POST['year']
+        #Reporte aprobados y reporbados en un semestre
         '''mon_str = {0:'Enero',1:'Febrero',2:'Marzo',3:'Abril',4:'Mayo',5:'Junio',6:'Julio',7:'Agosto',8:'Septiembre',9:'Ocutbre',10:'Noviembre',11:'Diciembre'}
         month = datetime.datetime.now().month
         mon_spanish = mon_str[month]'''
@@ -19,4 +21,21 @@ def dashboards(request):
     return render(request,'reportes.html',{'mes':mes,'periodo':periodo,'year':year,'regs':regs})
 
 def listados(request):
-    return render(request,'listados.html',{})
+    cursos = Curso.objects.all()
+    regs = 'NIL'
+    mes = 'No Seleccionado'
+    curso = 'No Seleccionado'
+    year = 'No Seleccionado'
+    cohortes_mes_dpto = []
+    cohortes_mes_ganaron = []
+    if request.method == 'POST':
+        mes = request.POST['mes']
+        curso = request.POST['curso']
+        year = request.POST['year']
+        cohortes_mes_dpto = fachada.list_est_curso_dpto(mes,curso,year) #Detalle de estudiantes de mes por dpto
+        cohortes_mes_ganaron = fachada.hist_estud_ganaron(mes,curso,year) #Historico estudiantes ganaron curso
+        if len(cohortes_mes_dpto) == 0 and len(cohortes_mes_ganaron):
+            regs='NO'
+        else:
+            regs='SI'
+    return render(request,'listados.html',{'regs':regs,'mes':mes,'year':year,'curso':curso,'detalle_dpto':cohortes_mes_dpto,'cursos':cursos,'historico_mes':cohortes_mes_ganaron})
